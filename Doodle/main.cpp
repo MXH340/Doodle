4bc201d04249df941761469b5f1264a0ccf9b740
mengx <1264235380@qq.com>
2020-04-29 09:20:15 +0800

    添加项目文件。

---
+++ Doodle/main.cpp
@@ -0,0 +1,19 @@
+#include<SFML/Graphics.hpp>
+#include<time.h>
+
+using namespace sf;
+
+RenderWindow app(VideoMode(520, 450), "´ò×©¿é");
+Texture t1, t2, t3, t4;
+
+void GameInit();
+
+int main()
+{
+
+}
+
+void GameInit()
+{
+	app.setFramerateLimit(60);
+}
